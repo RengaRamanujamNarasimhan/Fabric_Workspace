b8e734ca0da51468cfd843572dbc9f2a079e9332
RengaRamanujamNarasimhan <rengal@systechusa.com>
2024-12-16 21:40:45 +0530

    Update createFabricWorkspace.ps1

--- createFabricWorkspace.ps1
+++ createFabricWorkspace.ps1
@@ -8,6 +8,12 @@ Install-Module -Name MicrosoftPowerBIMgmt
 #Connect to PowerBI Services
 Connect-PowerBIServiceAccount
 
+#Get the capacity Name as parameter and assign it to a local variable
+$cpName = $pcpName
+
+#Get the Capacity ID based on capacity name supplied and assign it to a variable
+$cpID = (Get-PowerBICapacity -Scope Organization | Where-Object { $_.DisplayName -Eq "cpdataanalyticstest" } | Select-Object -ExpandProperty Id).ToString()
+
 #Get the workspace Name as parameter and assign it to a local variable
 $wspName = $pwspName
 
@@ -16,12 +22,6 @@ New-PowerBIWorkspace -Name $wspName
 
 #Get the ID of the workspace created and assign it to a variable
 $wspID = (Get-PowerBIWorkspace -Name $wspName | Select-Object -ExpandProperty Id).ToString()
-
-#Get the capacity Name as parameter and assign it to a local variable
-$cpName = $pcpName
-
-#Get the Capacity ID based on capacity name supplied and assign it to a variable
-$cpID = (Get-PowerBICapacity -Scope Organization | Where-Object { $_.DisplayName -Eq "cpdataanalyticstest" } | Select-Object -ExpandProperty Id).ToString()
 
 #assign the capacity to the workspace
 Set-PowerBIWorkspace -Scope Organization -Id $wspID -CapacityId $cpID
