b2e905890a3a7caaa719fd2540773a088f33d6d4
RengaRamanujamNarasimhan <rengal@systechusa.com>
2024-12-16 23:04:55 +0530

    Update createFabricWorkspace.ps1
    Added the control block for capacity check

--- createFabricWorkspace.ps1
+++ createFabricWorkspace.ps1
@@ -2,17 +2,25 @@ Param($pwspName,$pcpName)
 
 
 #Install the required Modules
-Install-Module -Name Az -Repository PSGallery -Force
-Install-Module -Name MicrosoftPowerBIMgmt
+Install-Module -Name Az -Repository PSGallery -Force -WarningAction SilentlyContinue
+Install-Module -Name MicrosoftPowerBIMgmt -WarningAction SilentlyContinue
 
 #Connect to PowerBI Services
 Connect-PowerBIServiceAccount
+
+$cpID = $null
 
 #Get the capacity Name as parameter and assign it to a local variable
 $cpName = $pcpName
 
 #Get the Capacity ID based on capacity name supplied and assign it to a variable
-$cpID = (Get-PowerBICapacity -Scope Organization | Where-Object { $_.DisplayName -Eq "cpdataanalyticstest" } | Select-Object -ExpandProperty Id).ToString()
+Try {
+$cpID = (Get-PowerBICapacity -Scope Organization | Where-Object { $_.DisplayName -Eq $cpName -and $_.State -Eq "Active" } | Select-Object -ExpandProperty Id).ToString()
+}
+Catch {
+	Write-Error 'The capacity does not exist. This could be because the capacity is not Active or you do not have a capacity administartor permissions.' 
+	Exit
+}  
 
 #Get the workspace Name as parameter and assign it to a local variable
 $wspName = $pwspName
@@ -25,3 +33,4 @@ $wspID = (Get-PowerBIWorkspace -Name $wspName | Select-Object -ExpandProperty Id
 
 #assign the capacity to the workspace
 Set-PowerBIWorkspace -Scope Organization -Id $wspID -CapacityId $cpID
+
