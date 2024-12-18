a711920677c8b31f2ad5c46c7d575cd3a14434d7
RengaRamanujamNarasimhan <rengal@systechusa.com>
2024-12-18 23:28:55 +0530

    Update createServicePrincipalPowerBIAccess.ps1

--- createServicePrincipalPowerBIAccess.ps1
+++ createServicePrincipalPowerBIAccess.ps1
@@ -1,6 +1,7 @@
 Param($pservicePrincipal)
 
 Install-Module -Name Az -Repository PSGallery -Force -WarningAction SilentlyContinue
+Import-Module Az.Accounts -Force
 Import-Module Az.Resources -Force
 
 #Create a Service Principal for login access
